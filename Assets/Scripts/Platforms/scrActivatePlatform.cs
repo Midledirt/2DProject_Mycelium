450979818d77be4d285a77bf143065b872d949c7
Jonathan <jonpres@icloud.com>
2020-05-19 16:18:15 +0200

    Added a ton of sprites, make the "Mushroom Platform" work with animation.

--- Assets/Scripts/Platforms/scrActivatePlatform.cs
+++ Assets/Scripts/Platforms/scrActivatePlatform.cs
@@ -7,9 +7,14 @@ public class scrActivatePlatform : MonoBehaviour
     [Range(1f, 10f)]
     public float platformTimer = 1f;
     public bool isTouched;
+    [Tooltip("This can be any cube with a 2D collider, as the mesh should be turned invisible.")]
     public GameObject platform;
     private GameObject instantiatedPlatform;
+    [Tooltip("This is the point where the invisible platform will appear when the trigger is touched.")]
     public GameObject platformPlacemeent;
+
+    //Animation
+    public Animator MushroomAnimator;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,6 +28,11 @@ public class scrActivatePlatform : MonoBehaviour
     {
         if (isTouched && instantiatedPlatform == null)
         {
+            //Make the touched animation start.
+            if (MushroomAnimator != null)
+            {
+                MushroomAnimator.SetBool("MushroomTouched", true);
+            }
             StartCoroutine(tempPlatform());
             //print(transform.name + "is activated");
             //reset touch to false
@@ -46,11 +56,13 @@ public class scrActivatePlatform : MonoBehaviour
         
             instantiatedPlatform = Instantiate(platform, platformPlacemeent.transform.position, Quaternion.identity);
         
-        
-        
             //platform.SetActive(true);
 
             yield return new WaitForSeconds(platformTimer);
+            if (MushroomAnimator != null)
+            {
+                MushroomAnimator.SetBool("MushroomTouched", false);
+            }
             if (platform != null)
             {
                 Destroy(instantiatedPlatform);
