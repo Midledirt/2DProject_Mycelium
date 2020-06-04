ecbe436fa01755ea7958b11e9e1693fe0b93640d
Jonathan <jonpres@icloud.com>
2020-06-04 13:52:02 +0200

    Made the spore occlusion object move back to its original position when the game restarts

---
+++ Assets/Scripts/scrOcclusionRespawn.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scrOcclusionRespawn : MonoBehaviour
+{
+    [Tooltip("This should contain the original startpoint object position")]
+    public Transform originalSpawnPoint;
+
+    private GameObject oGame;
+    private bool checkIfRespawn;
+
+
+    private void Start()
+    {
+        //Find the current instance of oGame
+        oGame = GameObject.FindGameObjectWithTag("oGame");
+
+        //Set respawn to false
+        checkIfRespawn = false;
+    }
+    void Update()
+    {
+        //The reset items function is called when the game restarts, thus we know the game is restarting when we get this component
+        checkIfRespawn = oGame.GetComponent<scrSpores>().resetItems;
+
+        if (checkIfRespawn == true)
+        {
+            //Set this object back to its start position
+            transform.position = originalSpawnPoint.position;
+        }
+    }
+}
