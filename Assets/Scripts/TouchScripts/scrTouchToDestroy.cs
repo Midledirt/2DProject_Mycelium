82a18cae6d20e7e257887833de937a1198d34450
Jonathan <jonpres@icloud.com>
2020-06-08 13:48:25 +0200

    Added a new animation that plays when a spore cloud is destroyed

--- Assets/Scripts/TouchScripts/scrTouchToDestroy.cs
+++ Assets/Scripts/TouchScripts/scrTouchToDestroy.cs
@@ -19,11 +19,19 @@ public class scrTouchToDestroy : MonoBehaviour
     private int health = 5;
     private int currentHealth;
 
+    //Animate death for objects
+    public bool hasDeathAnimation = false;
+    [SerializeField]
+    [Tooltip("Set this to the actual length of the deathAnimation")]
+    [Range(0.5f, 1f)]
+    public float animationTimer = 0.90f;
+
     [SerializeField]
     [Range(1, 5)]
     [Tooltip("How much damage this object takes when touched")]
     private int damageOnTouch = 1;
 
+    
     [Tooltip("Assign the animator component on this object")]
     public Animator animator;
     [Tooltip("Assign the renderer component on this object")]
@@ -75,19 +83,58 @@ public class scrTouchToDestroy : MonoBehaviour
     }
     private void DestroyObject()
     {
-        //Animate
-
-
         //print("I am dead");
-        gameObject.GetComponent<SpriteRenderer>().enabled = false;
-        isActive = false;
+        //Check if this object has a death animation
+        if (hasDeathAnimation == false)
+        {
+            gameObject.GetComponent<SpriteRenderer>().enabled = false;
+            isActive = false;
+        }
+        else if (hasDeathAnimation == true)
+        {
+            StartCoroutine(DeathAnimation());
+        }
+
     }
     IEnumerator onTakeDamge()
     {
         getRenderer.material.SetColor("_Color", Color.red);
-        //animator.SetBool("isTouched", true);
+        if (animator != null && animator.isActiveAndEnabled)
+        {
+            animator.SetBool("isTouched", true);
+        }
         yield return new WaitForSeconds(0.1f);
-        //animator.SetBool("isTouched", false);
+        if (animator != null && animator.isActiveAndEnabled)
+        {
+            animator.SetBool("isTouched", false);
+        }
         getRenderer.material.SetColor("_Color", Color.white);
+    }
+
+    IEnumerator DeathAnimation()
+    {
+        if (hasDeathAnimation == false)
+        {
+            yield return null;
+        }
+        else if (animator != null)
+        {
+            //Animate
+            //print("isDestroyedBegins");
+            if (animator != null && animator.isActiveAndEnabled)
+            {
+                animator.SetBool("isDestroyed", true);
+            }
+            yield return new WaitForSeconds(animationTimer);
+            //Destroy
+            gameObject.GetComponent<SpriteRenderer>().enabled = false;
+            isActive = false;
+            if (animator != null && animator.isActiveAndEnabled)
+            {
+                animator.SetBool("isDestroyed", false);
+            }
+            //print("isDestroyedEnds");
+        }
+
     }
 }
